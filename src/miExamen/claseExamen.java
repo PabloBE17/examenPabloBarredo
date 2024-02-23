d927765aea3433be23724eb4a6c66bc28159fcde
PabloBE17 <pablobe87@educastur.es>
2024-02-23 19:22:18 +0200

    Añadidos los primeros ficheros al proyecto

---
+++ src/miExamen/claseExamen.java
@@ -0,0 +1,13 @@
+
+ 
+package miExamen;
+
+
+public class claseExamen {
+
+    
+    public static void main(String[] args) {
+        System.out.println("Estoy haciendo el examen de la UT3");
+    }
+    
+}
