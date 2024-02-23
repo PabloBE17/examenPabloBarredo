1acee9050a1964c93769dcea409567302dc53543
PabloBE17 <pablobe87@educastur.es>
2024-02-23 19:32:06 +0200

    Añadidos comentarios de documentación en claseExamen.

--- src/miExamen/claseExamen.java
+++ src/miExamen/claseExamen.java
@@ -1,4 +1,12 @@
-
+/*
+Entornos de desarrollo UT3
+: Esta clase sirve para realizar el examen de la UT3 del módulo Entornos de
+Desarrollo
+: Curso de Git y github de píldoras informáticas
+Autor:Pablo Barredo
+Version:1.0.0
+Fecha de creación de la clase: 23/02/2024
+*/
  
 package miExamen;
 
